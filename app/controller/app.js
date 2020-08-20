33a5f14e8aa5d4f555acdc5cdc86813602daf25d
Zeke-D <zeke.dascoli@gmail.com>
2020-08-20 16:33:24 -0400

    Adding routers

---
+++ app/controller/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const api_v1 = require('./API/v1/routes.js')
+
+const app = express();
+// setup middleware
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.use('/api/v1', api_v1);
+
+app.get('/', (req, res) => {
+    res.json({
+        hello: "world"
+    })
+});
+
+
+// not found middleware
+app.use((req, res, next) => {
+    const err = new Error(`URL ${req.originalUrl} not found`);
+    res.status(404);
+    next(err);
+});
+
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: error.stack
+    });
+});
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {console.log("WOOHOO")});
