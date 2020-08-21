91f5c741079093e9fe1d5440544eb00381623f30
Zeke-D <zeke.dascoli@gmail.com>
2020-08-21 14:05:02 -0400

    Abstracting higher order db error handler

--- app/controller/API/v1/userCrud.js
+++ app/controller/API/v1/userCrud.js
@@ -1,30 +1,41 @@
 const express = require('express');
-const { Client } = require('pg');
-
-const client = new Client();
+const { db, errorHandlerCreator } = require('../../../model/db/db.js');
 const router = express.Router();
 
 
-client.connect();
+
 // CRUD for users
 
 // read all users
 router.get('/', async (req, res) => {
-    const query_res = await client.query('SELECT * from users');
+    const query_res = await db.query('SELECT * from users');
     res.json({
         user:query_res
     });
 });
 
 // read one user
-router.get('/:id', async (req, res) => {
-    const query_res = await client.query('SELECT * from users WHERE id is $1::integer', [req.params.id]);
-    res.json({
-        user:query_res
-    });
+router.get('/:id', async (req, res, next) => {
+    const { params: { id }} = req; // same as 'const id = req.params.id;'
+    console.log(id);
+    await db.query('SELECT * from users WHERE id = $1::integer', [id], errorHandlerCreator(res, next));
 });
 
 // create one user
+router.post('/', async (req, res) => {
+    const { 
+        body: {
+            firstName,
+            lastName,
+            email,
+            phone,
+            password
+        }
+    } = req;
+
+
+
+});
 
 // update one user
 
